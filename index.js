e51a3905210386d940e0d3bf9a13450fa6c4a308
Nikolay <75427338+korhenon@users.noreply.github.com>
2025-04-10 22:18:58 +0300

    change websocket

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 import { WebSocketServer } from 'ws';
 
 const wss = new WebSocketServer({ port: 8080 });
+const clients = []
+
 
 wss.on('connection', (ws) => {
+    clients.push(ws);
     console.log('Новый клиент подключился!');
 
-    ws.on('message', (message) => {
-        console.log(`Получено сообщение: ${message}`);
+    ws.on('message', (data, isBinary) => {
+        clients.forEach((client) => {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                client.send(String(data));
+            }
+        });
     });
-
-    setInterval(() => {
-        ws.send("Hello my user")
-    }, 10000)
 
     ws.on('close', () => {
         console.log('Клиент отключился');
